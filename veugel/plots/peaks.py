410cfab88f46a2f5539c9e1b8e40f54e125f0c2c
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2015-07-29 09:54:49 +0200

    Commented code, plotted all days

--- veugel/plots/peaks.py
+++ veugel/plots/peaks.py
@@ -1,6 +1,6 @@
 from collections import defaultdict, OrderedDict
 import functools
-from itertools import count
+from itertools import count, chain
 from matplotlib import pyplot
 from veugel import peakdetect
 from veugel.analyse import analyse
@@ -15,7 +15,7 @@ def to_bucket(value, bucket_size):
         if bucket <= value < bucket + bucket_size:
             return bucket
 
-def to_buckets(values, bucket_size=3):
+def to_buckets(values, bucket_size):
     filled_buckets = defaultdict(int)
     for value in values:
         bucket = to_bucket(value, bucket_size)
@@ -23,7 +23,7 @@ def to_buckets(values, bucket_size=3):
     # Convert our normal dictionary to a dictionary ordered by the bucket
     return OrderedDict(sorted(filled_buckets.items()))
 
-def detect_peaks(values, minimum_peak_size=50):
+def detect_peaks(values, minimum_peak_size):
     UP, DOWN = 1, 2
     direction = UP
 
@@ -40,11 +40,14 @@ def detect_peaks(values, minimum_peak_size=50):
                 direction = UP
         previous_value = value
 
+def day_aggregate(das_values):
+    """
+    Gegeven een reeks getallen, bepaal:
 
-def aggregate(veugel):
-    das_values = veugel.days[0].rows["duration_of_state"]
-
-    buckets = to_buckets(das_values)
+      1. De buckets (dat wil zeggen, aggregeer punten die dicht bij elkaar liggen en tel hoe vaak ze voorkomen)
+      2. De pieken van die buckets
+    """
+    buckets = to_buckets(das_values, bucket_size=3)
     bucket_keys, bucket_values = zip(*buckets.items())
 
     # Peaks are represented as a list of tuples, with the first element giving
@@ -53,18 +56,33 @@ def aggregate(veugel):
     peaks = [(bucket_keys[x_peak], y_peak) for x_peak, y_peak in peaks]
     peaks = OrderedDict(peaks)
 
+    # peaks: een dictionary van bucket (int) -> count (int)
+    # buckets: een dictionary van bucket (int) -> count(int)
+
     # Return an ordered dict of buckets, and their peaks
     return buckets, peaks
 
-def plot(das_values, peaks):
+
+def aggregate(veugel):
+    # @LINDA: Kies je dagen die je wil berekenen in de onderstaande constructie!
+    days = veugel.days # FILTER DUS HIER
+
+    aggregated = [(day.daynr, day_aggregate(day.rows["duration_of_state"])) for day in days]
+    aggregated = OrderedDict(aggregated)
+
+    # HACK: als we hier een enkel item teruggeven gaat analyse() over de zeik :<. We geven
+    # dus een lijst met een enkel item terug, wat vervolgens weer goed gaat in plot().
+    return [aggregated]
+
+def plot(aggregated):
     pyplot.figure()
 
-    keys = list(das_values.keys())
-
-    pyplot.plot(keys, [das_values[x] for x in keys])
-    pyplot.plot(list(peaks.keys()), list(peaks.values()), 'x')
+    for day, (buckets, peaks) in aggregated.items():
+        print(day)
+        pyplot.plot(list(buckets.keys()), list(buckets.values()))
+        pyplot.plot(list(peaks.keys()), list(peaks.values()), 'x')
 
     pyplot.show()
 
 if __name__ == "__main__":
-    analyse([next(iter(BROTHERS.keys()))], aggregator=aggregate, plotter=plot, plot_threaded=True)
+    analyse([list(BROTHERS.keys())[0]], aggregator=aggregate, plotter=plot, plot_threaded=True)
