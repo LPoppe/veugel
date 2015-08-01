6c281d17f19889afac3779959104120d130d195a
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2015-08-01 13:04:35 +0200

    Added ks_2samp

--- veugel/plots/stats.py
+++ veugel/plots/stats.py
@@ -1,9 +1,8 @@
+from collections import defaultdict, Counter, OrderedDict
+
 __author__ = 'linda'
 
 from scipy import stats
-from itertools import count
-import functools
-from matplotlib import pyplot
 from veugel import relational
 from veugel.analyse import analyse
 
@@ -11,22 +10,54 @@ BUCKETS = [50, 60, 70, 90, 120]
 MAX_DAYS = 1000
 N_BINS = 20
 
-@functools.lru_cache(MAX_DAYS)
-def to_quintuple(day):
-    for bucket in count(0, 5):
-        if bucket - 2.5 < day <= bucket + 2.5:
-            return bucket
+
+def choose_day(days, daynr, tries=3):
+    days = {day.daynr: day for day in days}
+    for day_delta in range(0, tries):
+        if daynr + day_delta in days:
+            return days[daynr + day_delta]
+        elif daynr - day_delta in days:
+            return days[daynr - day_delta]
+    raise ValueError("No key day found between (%s, %s)" % (daynr - tries, daynr + tries))
+
 
 def get_hist_data(veugel):
-    quintuple_map = {to_quintuple(day.daynr): day for day in veugel.days}
-    return [list(quintuple_map[n].get_gap_lengths()) for n in BUCKETS]
+    days = [choose_day(veugel.days, daynr) for daynr in BUCKETS]
+    days = [day.rows["duration_of_state"] for day in days]
+    days = [Counter(das_values) for das_values in days]
+    days = [OrderedDict(sorted(counted.items())) for counted in days]
+    return days
+
+
+def get_ks(iso, self):
+    for daynr in BUCKETS:
+        #print(sorted(Counter(choose_day(iso.days, daynr).rows["duration_of_state"]).items()))
+        #print(sorted(Counter(choose_day(self.days, daynr).rows["duration_of_state"]).items()))
+        iso_das = choose_day(iso.days, daynr).rows["duration_of_state"]
+        self_das = choose_day(self.days, daynr).rows["duration_of_state"]
+        yield stats.ks_2samp(iso_das, self_das)
+
+
+def _aggregate(iso, self):
+    yield iso.name
+    yield self.name
+
+    yield get_hist_data(iso)
+    yield get_hist_data(self)
+
+    yield list(get_ks(iso, self))
+
 
 def aggregate(iso, self):
-    return iso.name, list(get_hist_data(iso)), self.name, list(get_hist_data(self))
+    return list(_aggregate(iso, self))
 
-def plot(iso_name, iso_bins, self_name, self_bins):
-    kaas = stats.ks_2samp(iso_bins, self_bins)
-    print(kaas)
+
+def plot(iso_name, self_name, iso_hist, self_hist, ks):
+
+    print("{} <-> {}".format(iso_name, self_name))
+    for day, res in zip(BUCKETS, ks):
+        print("{}: {}".format(day, res))
+
 
 if __name__ == '__main__':
-    analyse(relational.BROTHERS.items(), plot, aggregate, plot_threaded=True)
+    analyse(list(relational.BROTHERS.items())[:1], plot, aggregate, plot_threaded=True)
